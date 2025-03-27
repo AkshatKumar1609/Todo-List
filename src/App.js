50832dec6b0488af30cf66d0550e307daa3f99ab
AkshatKumar1609 <akshatk.ug23.cs@nitp.ac.in>
2025-03-27 17:00:12 +0530

    added toast notification

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import { useState } from 'react';
 import './App.css';
+import { toast, ToastContainer } from 'react-toastify';
 
 function App() {
   let [todoData,setTodoData] = useState([{name:"yes",dueDate:2}]);
   return (
-    <>
-      <p className="title">Todo List</p>
-        <div className = "input-format">
-            <input placeholder="Todo Name" className="input-area"/>
-            <input type="date" className="input-date"/>
-            <button onClick={add} className="input-button">Add</button>
-        </div>
-        <div className="show">
-          {
-            todoData.map((item,i)=>{
-              return(
-                <Card todoItem = {item} i={i}/>
-              )
-            })
-          }
-        </div>
-    </>
+    <div tabIndex="0" onKeyDown={(event) => { if (event.key === 'Enter') add();}}>
+    <ToastContainer/>
+    <p className="title">Todo List</p>
+      <div className = "input-format">
+          <input placeholder="Todo Name" className="input-area"/>
+          <input type="date" className="input-date"/>
+          <button onClick={add} className="input-button">Add</button>
+      </div>
+      <div className="show">
+        {
+          todoData.map((item,i)=>{
+            return(
+              <Card todoItem = {item} i={i}/>
+            )
+          })
+        }
+      </div>
+    </div>
   );
 
   function Card({todoItem,i}){
@@ -40,16 +42,20 @@ function App() {
     if(name !== ""){
       if(!todoData.some(item => item.name === name)){
         setTodoData([...todoData, { name: name, dueDate: dueDate }]);
+        toast.success('added')
       } 
       else{
-        alert('same data');
+        toast.warn('same data');
       }
     }
-    console.log(todoData)
+    else{
+      toast.error('data empty');
+    }
   }
 
   function deleteItem(index) {
     setTodoData(todoData.filter((_, i) => i !== index));
+    toast.info('deleted')
   }
 }
 
